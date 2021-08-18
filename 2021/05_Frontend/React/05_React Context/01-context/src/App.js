a461ba2d462898b30debf56287d8ef6aeb529440
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-08-17 19:48:09 -0500

    basic App part (context)

---
+++ 2021/05_Frontend/React/05_React Context/01-context/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hola en app 💻</h1>
+
+      <Header />
+      <Main />
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
