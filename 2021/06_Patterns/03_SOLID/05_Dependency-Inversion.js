f6891663f4b943eecf4e28fc112f0b7f4f5a74fa
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-09-22 21:43:35 -0500

    Clase de SOLID (Codigo tomado de Sensei)

---
+++ 2021/06_Patterns/03_SOLID/05_Dependency-Inversion.js
@@ -0,0 +1,76 @@
+
+// Sin Principio
+
+class Alarm {
+    constructor() {
+
+    }
+
+    start() {
+        return `This is Ringing`
+    }
+
+    stop() {
+        return `The alarm has stopped`
+    }
+}
+
+class NiceHouse {
+    isBreak = false;
+    alarm = null;
+
+    constructor() {
+        this.alarm = new Alarm();
+    }
+
+    onBreak() {
+        if (this.isBreak) {
+            this.alarm.start();
+            this.isBreak = true;
+        } else {
+            this.alarm.stop();
+            this.isBreak = false;
+        }
+    }
+}
+
+
+// APLICANDO PRINCIPIO
+class Alarm {
+    start() {
+        return 'Soy una alarma y estoy sonando!!! ⏰';
+    }
+
+    stop() {
+        return 'Soy una alarma y me detuve 🤫';
+    }
+}
+
+class HouseAlarm extends Alarm {
+    start() {
+        return 'Soy una alarma y estoy sonando!!! ⏰';
+    }
+
+    stop() {
+        return 'Soy una alarma y me detuve 🤫';
+    }
+}
+
+class NiceHouse {
+    isBreak = false;
+    alarm = null;
+
+    constructor() {
+        this.alarm = new HouseAlarm();
+    }
+
+    onBreak() {
+        if (this.isBreak) {
+            this.alarm.start();
+            this.isBreak = true;
+        } else {
+            this.alarm.stop();
+            this.isBreak = false;
+        }
+    }
+}
