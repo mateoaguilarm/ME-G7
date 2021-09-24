65a0a3a474929dd63c0b9263c6dad84313d1574d
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-09-23 21:00:45 -0500

    Testing Class explanation 1/2

---
+++ 2021/06_Patterns/04_Testing/index.js
@@ -0,0 +1,10 @@
+const sum = require('./sum');
+const substract = require('./substract');
+
+const resultado15 = sum(10, 5);
+
+const resultadoResta = substract(15, 7)
+
+console.log(`The answer for the sum is: ${resultado15}`);
+
+console.log(`The answer for the substract is: ${resultadoResta}`);
