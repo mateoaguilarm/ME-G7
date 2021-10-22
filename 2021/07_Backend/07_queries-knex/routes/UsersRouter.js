0f2a290351795804247927d1ec6c603f0e75e4b6
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-10-21 20:32:13 -0500

    se mejora funcionalidad con controladores

--- 2021/07_Backend/07_queries-knex/routes/UsersRouter.js
+++ 2021/07_Backend/07_queries-knex/routes/UsersRouter.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { UsersController } = require('../controllers')
 
-router.get('/', (req, res) => {
-    res.status(200).send('MSG from Server (UsersRouter.js)')
-})
+router.get('/', UsersController.getAllUsers);
+router.post('/', UsersController.createUser);
 
 module.exports = router;
