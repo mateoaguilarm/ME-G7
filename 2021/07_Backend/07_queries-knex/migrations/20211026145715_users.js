528df9769b1587535f22bacefc9ebadd92b04eb0
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-10-26 10:44:01 -0500

    se settea para creation o drop de tabla users

--- 2021/07_Backend/07_queries-knex/migrations/20211026145715_users.js
+++ 2021/07_Backend/07_queries-knex/migrations/20211026145715_users.js
@@ -1,8 +1,14 @@
 
 exports.up = function (knex) {
     // Crear Tabla
+    return knex.schema.createTable('users', function (table) {
+        table.increments('user_id').notNullable().unique().primary();
+        table.string('email', 50).notNullable().unique();
+        table.string('password', 20).notNullable();
+    });
 };
 
 exports.down = function (knex) {
     // Eliminar Tabla
+    return knex.schema.dropTableIfExists('users')
 };
