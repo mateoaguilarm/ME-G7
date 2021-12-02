dee3020aa5725359e8a1cebaa8f3dba76e54765b
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-12-01 22:42:43 -0500

    ejercicios de NoSQL

---
+++ 2021/07_Backend/10_NoSQL/Ejercicios.js
@@ -0,0 +1,10 @@
+// Ejercicios NoSQL
+
+// Ejercicio 1 
+let peliculas = {
+    id: "01",
+    nombre: "Volver Al Futuro",
+    genero: ["Aventura", "Ficción"],
+    desc: "Lorem ipsum dolor sit amet, consectet. Lorem ipsum dolor sit amet, consectet"
+}
+
